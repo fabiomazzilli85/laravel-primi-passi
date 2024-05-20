ba366f94e1ee15e15fbec358a14e7699f8c08a36
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-05-20 16:42:03 +0200

    Bug-fix: pagina contact visualizzata.

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel Primi Passi | Contact</title<>
+    <title>Laravel Primi Passi | Contatti</title>
 </head>
 <body>
     @include('header')
-    
-    <h1>Scrivici quando vuoi</h1>
+
+    <h1>Scrivici</h1>
     
 </body>
 </html>
